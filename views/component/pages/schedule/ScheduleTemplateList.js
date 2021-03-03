80b4ced9822031083e0a8488fe37642283ee8209
Adam Don <adamdon89@gmail.com>
2021-03-03 13:31:19 +0000

    Added simpleside bar for overflow-auto ul

--- views/component/pages/schedule/ScheduleTemplateList.js
+++ views/component/pages/schedule/ScheduleTemplateList.js
@@ -31,6 +31,20 @@ export default
         <div class="card-header"><i class="fas fa-clipboard-list"></i> Schedule Template List</div>
         <div class="card-body">
 
+          <div data-simplebar data-simplebar-auto-hide="false" class="overflow-auto" style="max-height: 270px;">
+            <ul class="list-group ">
+              <li class="list-group-item bg-secondary">Content</li>
+              <li class="list-group-item bg-secondary">Content</li>
+              <li class="list-group-item bg-secondary">Content</li>
+              <li class="list-group-item bg-secondary">Content</li>
+              <li class="list-group-item bg-secondary">Content</li>
+              <li class="list-group-item bg-secondary">Content</li>
+              <li class="list-group-item bg-secondary">Content</li>
+              <li class="list-group-item bg-secondary">Content</li>
+            </ul>
+          </div>
+
+
           
         </div>
     </div>
