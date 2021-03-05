1c8ac225533184e6776a30193925c227edb4e131
Adam Don <adamdon89@gmail.com>
2021-03-05 12:12:44 +0000

    Added scheduleTemplateUpdate route

--- controllers/routes/api/scheduleTemplateUpdate.js
+++ controllers/routes/api/scheduleTemplateUpdate.js
@@ -1,5 +1,72 @@
+import mongoose from "mongoose";
+import validator from 'validator';
+import {User} from "../../../models/User.js";
+import {Schedule} from "../../../models/Schedule.js";
+
+import validateToken from "../../utilities/validateToken.js";
+
+
+
+
 export default async function (request, response)
 {
-    console.log("scheduleTemplateUpdate");
-    response.send("<h1>scheduleTemplateUpdate</h1>");
+    try
+    {
+        let token = request.body.token;
+        let schedule_id = request.body._id;
+        let title = request.body.title;
+
+
+        //Check that data has been sent
+        if((typeof token == "undefined") || (typeof schedule_id == "undefined") || (typeof title == "undefined")) //TODO try request.body.hasOwnProperty('token');
+        {
+            return response.status(400).json({errors: [{msg: "Missing data"}] });
+        }
+
+
+        //Check that data is valid
+        if (validator.isEmpty(schedule_id))
+        {
+            return response.status(400).json({errors: [{msg: "Schedule id is required"}] });
+        }
+        if (validator.isEmpty(title))
+        {
+            return response.status(400).json({errors: [{msg: "New title is required"}] });
+        }
+
+        //Check that token is valid
+        let validateResult = validateToken(token);
+        if(validateResult === "invalid")
+        {
+            return response.status(400).json({errors: [{msg: validateResult}] });
+        }
+
+
+        //Check user is in database
+        let user = await User.findById(validateResult.user.id).select("-password");
+        if(!user)
+        {
+            return response.status(400).json({errors: [{msg: "User not found"}] });
+        }
+
+
+        //Check schedule is in database
+        let schedule = await Schedule.findOne({_id: schedule_id, user: user.id});
+        if (!schedule)
+        {
+            return response.status(400).json({errors: [{msg: "Schedule not found"}]});
+        }
+
+
+        //Remove schedule is in database
+        schedule.title = title;
+        await schedule.save();
+
+        response.json(schedule);
+    }
+    catch (e)
+    {
+        console.error(e.message);
+        return response.status(500).json({errors: [{msg: "Server error"}]});
+    }
 }
