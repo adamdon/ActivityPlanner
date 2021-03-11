4045762a1471f2fce4026a38d3463f23da6d3251
Adam Don <adamdon89@gmail.com>
2021-03-11 10:33:33 +0000

    Added Calendar model

--- models/Calendar.js
+++ models/Calendar.js
@@ -1,1 +1,58 @@
-//stub
+import mongoose from "mongoose";
+
+
+/**
+ * @constructor Calendar
+ */
+const CalendarSchema = new mongoose.Schema
+(
+    {
+
+        user:
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'user'
+            },
+
+
+        schedules:
+            [
+                {
+                    schedule:
+                        {
+                            type: mongoose.Schema.Types.ObjectId,
+                            ref: 'schedule'
+                        },
+
+                    date:
+                        {
+                            type: Date,
+                        },
+                }
+            ],
+
+        achievements:
+            [
+                {
+                    achievement:
+                        {
+                            type: mongoose.Schema.Types.ObjectId,
+                            ref: 'achievement'
+                        },
+
+                    date:
+                        {
+                            type: Date,
+                            default: Date.now,
+                        },
+                }
+            ],
+
+
+
+
+
+    }
+);
+
+export let Calendar = mongoose.model('calendar', CalendarSchema);
