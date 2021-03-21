ebbf27f4925648cd501b49a75a50abd87168789b
Adam Don <adamdon89@gmail.com>
2021-03-21 11:32:20 +0000

    Added UI stub for CalendarResults

--- views/component/pages/calendar/CalendarPage.js
+++ views/component/pages/calendar/CalendarPage.js
@@ -3,6 +3,7 @@ import ContainerLayout from '../../containers/ContainerLayout.js'
 import ContainerContent from '../../containers/ContainerContent.js';
 import ContainerContentRow from '../../containers/ContainerContentRow.js';
 
+import CalendarResults from "./CalendarResults.js";
 import CalendarAssignment from "./CalendarAssignment.js";
 import CalendarAchievement from "./CalendarAchievement.js";
 
@@ -14,10 +15,13 @@ export default
     name: "CalendarPage",
 
 
-    components: {Navbar, ContainerLayout, ContainerContent, ContainerContentRow, CalendarAssignment, CalendarAchievement},
+    components: {Navbar, ContainerLayout, ContainerContent, ContainerContentRow, CalendarResults, CalendarAssignment, CalendarAchievement},
     template: `
     
     <ContainerLayout>
+    <ContainerContentRow>
+      <ContainerContent desktop="col-xl-12"><CalendarResults></CalendarResults></ContainerContent>
+    </ContainerContentRow>
         <ContainerContentRow>
           <ContainerContent desktop="col-xl-6"><CalendarAssignment></CalendarAssignment></ContainerContent>
           <ContainerContent desktop="col-xl-6"><CalendarAchievement></CalendarAchievement></ContainerContent>
---
+++ views/component/pages/calendar/CalendarResults.js
@@ -0,0 +1,78 @@
+export default {
+    name: "CalendarResults",
+
+    data()
+    {
+        return {
+            calendar: null,
+        };
+    },
+
+    methods:
+        {
+
+            async updateResults()
+            {
+                const token = localStorage.getItem("token");
+
+
+                if (token)
+                {
+                    let requestBody = {token: token,};
+                    let requestUrl = "/api/scheduleUserAllRead";
+                    let requestHeaders = {"Content-Type": "application/json"};
+
+                    const response = await fetch(requestUrl, {method: "POST", headers: requestHeaders, body: JSON.stringify(requestBody)});
+                    const data = await response.json();
+                    if ((data) && (!data.errors))
+                    {
+                        console.log(data);
+                        this.calendar = data;
+                        // this.schedules = data;
+                    } else
+                    {
+                        console.log(data);
+                        // this.errorAlert = "Error: " + data.errors[0].msg;
+                    }
+                } else
+                {
+                    this.$router.push("/user");
+                }
+
+
+            },
+
+
+
+        },
+
+
+
+
+    async mounted()
+    {
+        await this.updateResults();
+
+    },
+
+
+
+
+    template: `
+      <div class="card text-white bg-primary">
+          <div class="card-header"><i class="fas fa-poll-h"></i> Calendar Results</div>
+          <div class="card-body">
+
+
+            <!----------- content start ---------------->
+            
+            test
+
+            <!----------- content end ---------------->
+
+
+
+          </div>
+      </div>
+    `,
+};
