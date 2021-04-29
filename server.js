af2b7b9bffa94e21674eef2069bb01d678f3d322
Adam Don <adamdon89@gmail.com>
2021-04-29 20:12:12 +0100

    Disabled livereload for deployment

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 import dotenv from "dotenv/config.js";
 import express from "express";
 import history from "connect-history-api-fallback";
-import livereload from "livereload";
-import connectLivereload from "connect-livereload";
+// import livereload from "livereload";
+// import connectLivereload from "connect-livereload";
 import {router} from "./controllers/router.js";
 import {database} from "./controllers/database.js";
 
 
 
-//Sets up livereload so changes to html will auto refresh browser without plugins
-let livereloadServer = livereload.createServer({extraExts : ["vue"]});
-
-livereloadServer.watch("views");
-livereloadServer.server.once("connection", () => {setTimeout(() => {livereloadServer.refresh("/");}, 100);});
+// //Sets up livereload so changes to html will auto refresh browser without plugins
+// let livereloadServer = livereload.createServer({extraExts : ["vue"]});
+//
+// livereloadServer.watch("views");
+// livereloadServer.server.once("connection", () => {setTimeout(() => {livereloadServer.refresh("/");}, 100);});
 
 
 //Connecting to database using process.env.MONGO_URI environment variables
@@ -22,7 +22,7 @@ await database.connect();
 //Set request handles
 let expressApp = express();
 
-expressApp.use(connectLivereload()); //monkey patches HTML with livereload.js for auto F5
+// expressApp.use(connectLivereload()); //monkey patches HTML with livereload.js for auto F5
 expressApp.use(history());
 expressApp.use(express.json());
 expressApp.use(express.static("views"));
